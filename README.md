83ea6036db2aeff2a840e055058af3aebbb11857
Kasper Sundström <kasper.sundstrom@gmail.com>
2016-07-27 02:31:48 +0300

    Första bulkuppladdning

---
+++ README.md
@@ -0,0 +1,38 @@
+# [KoalaOnline](http://koala.ksundstrom.fi)
+
+KoalaOnline är ett webbverktyg för Akademens LSM-tentamina, inspirerat av det tidigare Javaverktyget Koala _(Kind-of-a-Lottery App)_, bägge kodade av Kasper Sundström.
+
+
+## Uppdatering av sångerna
+
+* Sångerna läses in från de tre textfilerna i mappen `/data/`.
+* I filerna måste varje sång med tillhörande info finnas på exakt en egen rad. Det får inte finnas några som helst tomma rader i textfilerna.
+* Använd riktiga/typografiska citationstecken, apostrofer, ellipser och andra specialtecken.
+* Filerna måste vara ren text (`TXT`).
+* Ta backup på filerna först om du ämnar uppdatera dem.
+* Öppna filen/filerna i `/data/` i en riktig text/kodeditor, exempelvis Notepad++. Om du i Windows öppnar i vanliga Anteckningar kan det hända att radbrytningarna inte syns.
+* Uppdatera vad som måste uppdateras. Använd vid behov hjälpfilen `kodgenerator-lsm-2015.xlsx` (fråga upphovsmannen) för att enkelt ta fram helt nya kodlistor. Kopiera kodlistorna från Excel (en kategori åt gången) och ersätt innehållet i respektive textfil.
+* Kontrollera att varje sång finns med och finns på egen rad. _Ta bort eventuella tomma rader i början och slutet!_
+* Spara filen/filerna som `TXT`, i `UTF8`, med `LF`- eller `CRLF`-radbrytningar (bägge fungerar). _Ändra inte filnamnen eller ändelserna!_
+
+
+## Upphovsman
+
+**[Kasper Sundström](https://twitter.com/ksundstrom)**
+
+
+## Tack
+
+Tack till **André Brunnsberg** för hjälp med det första utkastet till HTML/JavaScript/jQuery samt förstås till **[Akademen](http://akademen.com)** som är den tänkta användaren och som inspirerat designen.
+
+
+## Framework
+
+KoalaOnline använder **[Bootstrap](http://getbootstrap.com)**.
+
+
+## Copyright
+
+Copyright © 2016 Kasper Sundström
+
+All rights retained where applicable. You may not without explicit permission reproduce, distribute, or create derivative works from this work.
---
+++ js/main.js
@@ -0,0 +1,150 @@
+// TURN OFF ANIMATIONS ON XS DEVICES
+
+if ( window.matchMedia( '(max-width: 767px)' ).matches ) {
+    $.fx.off = true;
+};
+
+
+// HELPER FUNCTIONS
+
+var getLocalTime = function() {
+    var d = new Date();
+    return d.toLocaleTimeString();
+};
+
+var textToArray = function( aText ) {
+    return aText.split(  /\r\n|\n/ );
+};
+
+var getRandomIndex = function( aArray ) {
+    return Math.floor( Math.random() * aArray.length );
+};
+
+
+// RETRIEVE DATA WITH AJAX AND POPULATE ARRAYS
+
+var urlA = 'data/lsm-a.txt',
+    urlB1 = 'data/lsm-b1.txt',
+    urlB2 = 'data/lsm-b2.txt';
+
+var songArrayA = [],
+    songArrayB1 = [],
+    songArrayB2 = [];
+
+var jqxhrA = $.get( urlA, $.noop, 'text' )
+    .done(function( data ) {
+        songArrayA = textToArray( data );
+        console.log( getLocalTime() + ': Läste in A' );
+    })
+    .fail(function() {
+        console.log( getLocalTime() + ': Kunde inte läsa in A' );
+        alert( 'Fel: inläsning av A-sångerna misslyckades!' );
+    });
+
+var jqxhrB1 = $.get( urlB1, $.noop, 'text' )
+    .done(function( data ) {
+        songArrayB1 = textToArray( data );
+        console.log( getLocalTime() + ': Läste in B1' );
+    })
+    .fail(function() {
+        console.log( getLocalTime() + ': Kunde inte läsa in B1' );
+        alert( 'Fel: inläsning av lättare B-sångerna misslyckades!' );
+    });
+
+var jqxhrB2 = $.get( urlB2, $.noop, 'text' )
+    .done(function( data ) {
+        songArrayB2 = textToArray( data );
+        console.log( getLocalTime() + ': Läste in B2' );
+    })
+    .fail(function() {
+        console.log( getLocalTime() + ': Kunde inte läsa in B2' );
+        alert( 'Fel: inläsning av svårare B-sångerna misslyckades!' );
+    });
+
+
+// IMPLEMENT BUTTON FUNCTIONS
+
+var buttonFunctions = function() {
+    var timeIcon = '<span class="glyphicon glyphicon-time" aria-hidden="true"></span> ';
+    
+    $( '#songButtonA' ).click(function() {
+        var song = songArrayA[ getRandomIndex( songArrayA ) ];
+        var time = getLocalTime();
+        console.log( time + ': A: ' + song );
+        $( '<div class="panel panel-success songResult">' +
+                '<div class="panel-heading">' +
+                    '<h3 class="panel-title clearfix">' +
+                        '<span class="glyphicon glyphicon-music" aria-hidden="true"></span> A-sång' +
+                        '<span class="pull-right">' + timeIcon + time + '</span>' +
+                    '</h3>' +
+                '</div>' +
+                '<div class="panel-body">' +
+                    song +
+                '</div>' +
+            '</div>' )
+            .hide()
+            .prependTo( '#songResults' )
+            .slideDown( 200 );
+    });
+    
+    $( '#songButtonB1' ).click(function() {
+        var song = songArrayB1[ getRandomIndex( songArrayB1 ) ];
+        var time = getLocalTime();
+        console.log( time + ': B1: ' + song );
+        $( '<div class="panel panel-warning songResult">' +
+                '<div class="panel-heading">' +
+                    '<h3 class="panel-title clearfix">' +
+                        '<span class="glyphicon glyphicon-music" aria-hidden="true"></span> Lättare B-sång' +
+                        '<span class="pull-right">' + timeIcon + time + '</span>' +
+                    '</h3>' +
+                '</div>' +
+                '<div class="panel-body">' +
+                    song +
+                '</div>' +
+            '</div>' )
+            .hide()
+            .prependTo( '#songResults' )
+            .slideDown( 200 );
+    });
+    
+    $( '#songButtonB2' ).click(function() {
+        var song = songArrayB2[ getRandomIndex( songArrayB2 ) ];
+        var time = getLocalTime();
+        console.log( time + ': B2: ' + song );
+        $( '<div class="panel panel-danger songResult">' +
+                '<div class="panel-heading">' +
+                    '<h3 class="panel-title clearfix">' +
+                        '<span class="glyphicon glyphicon-music" aria-hidden="true"></span> Svårare B-sång' +
+                        '<span class="pull-right">' + timeIcon + time + '</span>' +
+                    '</h3>' +
+                '</div>' +
+                '<div class="panel-body">' +
+                    song +
+                '</div>' +
+            '</div>' )
+            .hide()
+            .prependTo( '#songResults' )
+            .slideDown( 200 );
+    });
+    
+    $( '#helpButton' ).popover( {
+        placement: 'auto',
+        html: true,
+        content: '<p>Använd knapparna ovan för att lotta fram sånger ur de olika LSM-kategorierna. Den senast framlottade sången dyker upp högst upp. </p><p>Uppe till höger framgår när respektive sång lottades fram.</p><p>Resultaten finns kvar på skärmen tills du klickar på <i>Töm</i>.</p><p>Allt som gjorts under en session skrivs även ut med klockslag i webbläsarens konsol. Informationen finns kvar där tills sidan stängs.</p>'
+    });
+    
+    $( '#resetButton' ).click(function() {
+        var time = getLocalTime();
+        console.log( time + ': Raderade resultaten' );
+        $( '#songResults' ).animate( {
+            opacity: 'toggle',
+            height: 'toggle'
+        }, 200, 'swing', function() {
+            $( this )
+                .empty()
+                .show();
+        });
+    });
+};
+
+$( document ).ready( buttonFunctions );
