169c16f3d305ef03e992bd710f7642e86c4ff46b
Kasper Sundström <kasper.sundstrom@gmail.com>
2016-07-28 02:19:24 +0300

    Various updates

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-// TURN OFF ANIMATIONS ON XS DEVICES
-
+/**
+*   TURN OFF ANIMATIONS ON XS DEVICES
+*/
 if ( window.matchMedia( '(max-width: 767px)' ).matches ) {
     $.fx.off = true;
 };
 
-
-// HELPER FUNCTIONS
-
+/**
+*   HELPER FUNCTIONS
+*/
 var getLocalTime = function() {
     var d = new Date();
     return d.toLocaleTimeString();
@@ -20,9 +21,9 @@ var getRandomIndex = function( aArray ) {
     return Math.floor( Math.random() * aArray.length );
 };
 
-
-// RETRIEVE DATA WITH AJAX AND POPULATE ARRAYS
-
+/**
+*   RETRIEVE DATA WITH AJAX AND POPULATE ARRAYS
+*/
 var urlA = 'data/lsm-a.txt',
     urlB1 = 'data/lsm-b1.txt',
     urlB2 = 'data/lsm-b2.txt';
@@ -32,45 +33,45 @@ var songArrayA = [],
     songArrayB2 = [];
 
 var jqxhrA = $.get( urlA, $.noop, 'text' )
-    .done(function( data ) {
-        songArrayA = textToArray( data );
-        console.log( getLocalTime() + ': Läste in A' );
-    })
-    .fail(function() {
-        console.log( getLocalTime() + ': Kunde inte läsa in A' );
-        alert( 'Fel: inläsning av A-sångerna misslyckades!' );
-    });
+.done(function( data ) {
+    songArrayA = textToArray( data );
+    // console.log( getLocalTime() + ': Läste in A' );
+})
+.fail(function() {
+    // console.log( getLocalTime() + ': Kunde inte läsa in A' );
+    alert( 'Fel: inläsning av A-sångerna misslyckades!' );
+});
 
 var jqxhrB1 = $.get( urlB1, $.noop, 'text' )
-    .done(function( data ) {
-        songArrayB1 = textToArray( data );
-        console.log( getLocalTime() + ': Läste in B1' );
-    })
-    .fail(function() {
-        console.log( getLocalTime() + ': Kunde inte läsa in B1' );
-        alert( 'Fel: inläsning av lättare B-sångerna misslyckades!' );
-    });
+.done(function( data ) {
+    songArrayB1 = textToArray( data );
+    // console.log( getLocalTime() + ': Läste in B1' );
+})
+.fail(function() {
+    // console.log( getLocalTime() + ': Kunde inte läsa in B1' );
+    alert( 'Fel: inläsning av lättare B-sångerna misslyckades!' );
+});
 
 var jqxhrB2 = $.get( urlB2, $.noop, 'text' )
-    .done(function( data ) {
-        songArrayB2 = textToArray( data );
-        console.log( getLocalTime() + ': Läste in B2' );
-    })
-    .fail(function() {
-        console.log( getLocalTime() + ': Kunde inte läsa in B2' );
-        alert( 'Fel: inläsning av svårare B-sångerna misslyckades!' );
-    });
+.done(function( data ) {
+    songArrayB2 = textToArray( data );
+    // console.log( getLocalTime() + ': Läste in B2' );
+})
+.fail(function() {
+    // console.log( getLocalTime() + ': Kunde inte läsa in B2' );
+    alert( 'Fel: inläsning av svårare B-sångerna misslyckades!' );
+});
 
-
-// INITIALIZE BUTTONS
-
+/**
+*   INITIALIZE BUTTONS
+*/
 var buttonFunctions = function() {
     var timeIcon = '<span class="glyphicon glyphicon-time" aria-hidden="true"></span> ';
-    
+
     $( '#songButtonA' ).click(function() {
         var song = songArrayA[ getRandomIndex( songArrayA ) ];
         var time = getLocalTime();
-        console.log( time + ': A: ' + song );
+        // console.log( time + ': A: ' + song );
         $( '<div class="panel panel-success songResult">' +
                 '<div class="panel-heading">' +
                     '<h3 class="panel-title clearfix">' +
@@ -86,11 +87,11 @@ var buttonFunctions = function() {
             .prependTo( '#songResults' )
             .slideDown( 200 );
     });
-    
+
     $( '#songButtonB1' ).click(function() {
         var song = songArrayB1[ getRandomIndex( songArrayB1 ) ];
         var time = getLocalTime();
-        console.log( time + ': B1: ' + song );
+        // console.log( time + ': B1: ' + song );
         $( '<div class="panel panel-warning songResult">' +
                 '<div class="panel-heading">' +
                     '<h3 class="panel-title clearfix">' +
@@ -106,11 +107,11 @@ var buttonFunctions = function() {
             .prependTo( '#songResults' )
             .slideDown( 200 );
     });
-    
+
     $( '#songButtonB2' ).click(function() {
         var song = songArrayB2[ getRandomIndex( songArrayB2 ) ];
         var time = getLocalTime();
-        console.log( time + ': B2: ' + song );
+        // console.log( time + ': B2: ' + song );
         $( '<div class="panel panel-danger songResult">' +
                 '<div class="panel-heading">' +
                     '<h3 class="panel-title clearfix">' +
@@ -126,13 +127,15 @@ var buttonFunctions = function() {
             .prependTo( '#songResults' )
             .slideDown( 200 );
     });
-    
+
     $( '#helpButton' ).popover( {
         placement: 'auto',
         html: true,
-        content: '<p>Använd knapparna ovan för att lotta fram sånger ur de olika LSM-kategorierna. Den senast framlottade sången dyker upp högst upp. </p><p>Uppe till höger framgår när respektive sång lottades fram.</p><p>Resultaten finns kvar på skärmen tills du klickar på <i>Töm</i>.</p><p>Allt som gjorts under en session skrivs även ut med klockslag i webbläsarens konsol. Informationen finns kvar där tills sidan stängs.</p>'
+        content: '<p>Använd knapparna ovan för att lotta fram sånger ur de olika LSM-kategorierna. Den senast framlottade sången dyker upp högst upp. </p><p>Uppe till höger framgår när respektive sång lottades fram.</p><p>Resultaten finns kvar på skärmen tills du klickar på <i>Töm</i>.' +
+            // ' Allt som gjorts under en session skrivs även ut med klockslag i webbläsarens konsol. Informationen finns kvar där tills sidan stängs.</p>' +
+            '</p>'
     });
-    
+
     $( '#resetButton' ).click(function() {
         var time = getLocalTime();
         console.log( time + ': Raderade resultaten' );
@@ -146,5 +149,4 @@ var buttonFunctions = function() {
         });
     });
 };
-
 $( document ).ready( buttonFunctions );
